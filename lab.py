8f7af1bc4cfa52e2eef1eca97e2d3ab50e952bb3
hlebu5hek <74304719+hlebu5hek@users.noreply.github.com>
2024-05-10 11:54:25 +0400

    Update lab.py

--- lab.py
+++ lab.py
@@ -3,7 +3,7 @@
  Написать программу сравнительного вычисления данной функции рекурсивно и итерационно.
   Определить границы применимости рекурсивного и итерационного подхода.
    Результаты сравнительного исследования времени вычисления представить в табличной и графической форме в виде отчета по лабораторной работе.
-15.	F(1) = 1; G(1) = 1; F(n) = (-1)n*(F(n–1) – G(n–1))/(n+5)!, G(n) = F(n–1) + sin(n), при n >=2"""
+16) F(1) = 1; G(1) = 1; F(n) = (-1)n*(F(n–1) – G(n–1))/(2n)!, G(n) = 2*F(n–1) // G(n–1), при n >=2"""
 
 import math
 import time
@@ -16,19 +16,18 @@ timer=[]
 timer_rec=[]
 
 def factrial(x):
-    s = 1
-    for i in range(2, x+1):
-        s*=i
-    return s
+    if x == 1:
+        return x
+    else:
+        return x * factrial(x - 1)
 
 #рекурсия
-
 lru_cache(maxsize=None)
 def rec_f(x):
     if x < 2:
         return 1
     else:
-        return (-1)**int(n*(rec_f(x-1) - rec_g(x-1))/factrial(2*n))
+        return (-1)**n*(rec_f(x-1) - rec_g(x-1))/factrial(2*n)
 
 lru_cache(maxsize=None)
 def rec_g(x):
@@ -42,8 +41,8 @@ def it_f(x):
     f=[1]*3
     g=[1]*3
     for i in range(2,x+1):
-        g[1] = 2*f[0]//g[0] #либо cata_f[0]
-        f[-1] = (-1)**int(n*(f[0] - g[0])/factrial(2*n)) #либо Х
+        g[1] = 2 * f[0] // g[0]
+        f[-1] = (-1)**n*(f[0] - g[0])/factrial(2*n)
         f[0], f[1] = f[1], f[2]
         g[0], g[1] = g[1], g[2]
 
@@ -52,7 +51,6 @@ def it_f(x):
 while n < 1:
     print("Введите натуральное число от 1 ")
     n = int(input())
-
 
 graf = list(range(1, n+1))
 
@@ -67,10 +65,10 @@ for i in graf:
     timer_rec.append(end_rec-start_rec)
     rec_times = end_rec-start_rec
     iter_times = end-start
-    print("\n",i,"№результат рекурсии ",
-          res,"---------результат итерации",
-          result,"-----------время  РЕКУРСИИ ",
-          end_rec-start_rec,"-------время  ИТЕРАЦИИ",end-start, sep='\n')
+    print("\n",i,"  Результат рекурсии\n",
+          res,"  < - результат итерации\n",
+          result,"  < - время  рекурсии\n",
+          end_rec-start_rec,"  < - время  итерации\n",end-start)
 
 plt.plot(graf, timer, label='Итерационная функция.')
 plt.plot(graf, timer_rec, label='Рекусионная функция.')
